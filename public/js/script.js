e02af6e2615578a6cbc68008732226f2b0c58a22
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2025-05-04 08:24:48 +0700

    first-commit

---
+++ public/js/script.js
@@ -0,0 +1,55 @@
+// Dark Toogle
+darkToggle = document.getElementById('dark-toggle');
+html = document.querySelector('html');
+buttonDarkToggle = darkToggle.firstElementChild;
+
+darkToggle.addEventListener('click', function () {
+    if (html.classList.contains('dark')){
+        html.classList.remove('dark');
+        buttonDarkToggle.classList.remove('translate-x-4');
+        localStorage.theme = "light";
+    } else {
+        html.classList.add('dark');
+        buttonDarkToggle.classList.add('translate-x-4');
+        localStorage.theme = "dark";
+    }
+});
+
+if (html.classList.contains('dark')){
+    buttonDarkToggle.classList.add('translate-x-4');
+}
+
+// Navbar Fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.getElementById('to-top');
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.add('flex');
+        toTop.classList.remove('hidden');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+}
+
+// Hamburger Menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+// Klik di luar hamburger
+
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
